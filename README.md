17a5f3a28483661b588586ae3657d505d2a07dfb
unenglishable <unenglishable@gmail.com>
2014-08-30 17:44:13 -1000

    update test and include a README

---
+++ README.md
@@ -0,0 +1,13 @@
+# ziffy
+
+## Usage
+
+````
+var ziffy = require('ziffy');
+
+ziffy.convert('Hello World');
+
+...
+
+ziffy.transform(someStream).pipe(process.stdout);
+````
--- test/testZiffy.js
+++ test/testZiffy.js
@@ -2,10 +2,14 @@ var path = require('path');
 var fs = require('fs');
 var ziffy = require(path.join('..','ziffy'));
 
-var hello = ziffy.convert('Hello World');
+var hello = ziffy.convert('ziffy');
 console.log(hello);
 
 if (process.argv[2]) {
   var fileStream = fs.createReadStream(process.argv[2]);
-  ziffy.transform(fileStream).pipe(process.stdout);
+  var writeStream = process.stdout;
+  if (process.argv[3]) {
+    writeStream = fs.createWriteStream(process.argv[3]);
+  }
+  ziffy.transform(fileStream).pipe(writeStream);
 }
